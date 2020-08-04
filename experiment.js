954d56867a754aee536ea9bce6e1055eddf7656f
Michael Ellis <mchlpoulsen@gmail.com>
2020-08-04 13:17:50 +0200

    else if statement

--- experiment.js
+++ experiment.js
@@ -1,10 +1,12 @@
-//implementing control and equality operators to carry out logic, starting out with an if statement and expression.
+//an 'else if' statement into the 'if' statement, an additional condition for determining evaluated statements.
 
-const areTheLightsOn = true;
+const friendsAtYourParty = 2;
 
-if (areTheLightsOn) {
-    //turn the light off
-}  else {
-    //turn the light on
+if (friendsAtYourParty === 0) {
+    console.log("Cool, now I have all the nachos to myself...");
+} else if (friendsAtYourParty <= 4) {
+    console.log("Perfect amount to play some Mario Kart");
+} else {
+    console.log("Wooooo, play the dance music!");
 }
 

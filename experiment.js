26f7e8302f669fcaf656249b9d7eb2fdebacb497
Michael Ellis <mchlpoulsen@gmail.com>
2020-08-04 15:14:48 +0200

    adding comments

--- experiment.js
+++ experiment.js
@@ -3,6 +3,16 @@
 let friendsAtYourParty = 0;
 
 while(friendsAtYourParty < 10) {
+    //this is some comment to my future self
+
+    /*
+    everything in here is a comment
+
+    */
+
+   // friendsAtYourParty = friendsAtYourParty + 1;
+
+
     console.log(friendsAtYourParty);
     friendsAtYourParty = friendsAtYourParty + 1;
 }

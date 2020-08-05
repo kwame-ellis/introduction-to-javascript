a6488037e245dc6d4ca62d6193ffaccc51ef11d1
Michael Ellis <mchlpoulsen@gmail.com>
2020-08-05 18:04:54 +0200

    Quiz Exercise solution with numbers

--- experiment.js
+++ experiment.js
@@ -3,7 +3,7 @@ let points = 0;
 function australia(userAnswer) {
   // what is the capital of australia?
   // 100 pts
-  const isCorrect = userAnswer.toUpperCase === "CANBERRA";
+  const isCorrect = userAnswer === "Canberra";
   
   if (isCorrect) {
     points += 100; 
@@ -16,7 +16,7 @@ function canada(userAnswer) {
   // what is the capital of canada?
   // 100 pts
 
-  const isCorrect = userAnswer.toUpperCase === "OTTAWA";
+  const isCorrect = userAnswer  === "Ottawa";
 
   if(isCorrect) {
       points += 200;
@@ -41,8 +41,16 @@ function uppercase(userAnswer, word) {
 function firstThreeLetters(userAnswer, word) {
   // what are the first three letters of <word>?
   // 200 pts
-  
+
+  const isCorrect = userAnswer === word.substring(0, 3);
+
+  if(isCorrect) {
+      points += 200;
+  }
+
+  return isCorrect;
 }
+  
 
 function squared(userAnswer, number) {
   // what is <number> squared?
@@ -50,29 +58,56 @@ function squared(userAnswer, number) {
   
   const isCorrect = userAnswer == number ** 2;
 
+  if(isCorrect) {
+      points += 200;
+  }
+
+  return isCorrect;
 }
 
 function multiplication(userAnswer, num1, num2) {
   // what is <num1> multiplied by <num2>?
   // 300 pts
 
-  
+  const isCorrect = userAnswer == num1 * num2;
+
+  if(isCorrect) {
+      points += 300;
+  }
+
+return isCorrect;  
 }
 
 function age(userAnswer, currentYear, birthYear) {
   // if someone was born in <birthYear> and already has had their birthday this year, how old are they?
   // 300 pts
 
+  const isCorrect = currentYear - birthYear == userAnswer;
+
+  if(isCorrect) {
+      points += 300;
+  }
+
+  return isCorrect;
 }
 
 function larger(userAnswer, num1, num2) {
   // which of ${number4} and ${number5} is larger (if they're the same then pick that number)?
   // 300 pts
+
+  const isCorrect = Math.max(num1, num2) == userAnswer;
+
+  if(isCorrect) {
+      points += 300;
+  }
   
+  return isCorrect;
 }
 
 function getScore() {
   // returns the current user's quiz score score
+
+  return points;
 
 }
 
